991cecbca8e8905d362ab100de1f681e074f2067
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2025-07-17 16:41:35 +0600

    module - 07 - more operations for singly linked list

---
+++ Module - 07 - More Operations for Singly Linked List/delete_at_tail.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+    public:
+    int val;
+    Node* next;
+    
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node*  &tail, int val){
+    Node* newNode = new Node(val);
+    if (head == NULL)   
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    
+    tail->next = newNode;
+    tail = newNode;
+    
+};
+
+void delete_at_tail(Node* head, Node* &tail, int idx){
+    Node* temp = head;
+    for (int i = 1; i < idx; i++)
+    {
+        temp = temp->next;
+    }
+    Node* deleteNode = temp->next;
+
+    temp->next = temp->next->next;
+    delete deleteNode;
+    tail = temp;
+}
+
+
+void print_linked_list(Node* head){
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    };
+    
+};
+
+int main()
+{ 
+    Node* head = NULL;
+    Node* tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if(val == -1){
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    cout << "tail before delete:" << tail->val << endl;
+    delete_at_tail(head,tail, 4);
+    
+    print_linked_list(head);
+    cout << "tail after delete:" << tail->val << endl;
+    return 0;
+}
