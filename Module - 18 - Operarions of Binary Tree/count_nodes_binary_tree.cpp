20c1394177b9910332bf5cffc8e9e5d878485495
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2025-08-17 12:33:24 +0600

    module - 18 - operations of binary tree

---
+++ Module - 18 - Operarions of Binary Tree/count_nodes_binary_tree.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+class Node{
+    public:
+    int val;
+    Node* left;
+    Node* right;
+    
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* input_order(){
+    int val;
+    cin >> val;
+    Node* root = new Node(val);
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        Node* f = q.front();
+        q.pop();
+
+    int l, r;
+    cin >> l >> r;
+    Node* myLeft, *myRight;
+    if(l == -1) myLeft = NULL;
+    else myLeft = new Node(l);
+    if(r == -1) myRight = NULL;
+    else myRight = new Node(r);
+
+    f->left = myLeft;
+    f->right = myRight;
+
+
+    if(f->left) q.push(f->left);
+    if(f->right) q.push(f->right);
+
+    }
+    return root;
+}
+
+
+
+void levelOrder(Node* root){
+
+  queue<Node* > q;
+  q.push(root);
+  while(!q.empty()){
+    Node* f = q.front();
+    q.pop();
+    cout << f->val << " ";
+    if(f->left) q.push(f->left);
+    if(f->right) q.push(f->right);
+  }
+}
+
+
+
+int count_node(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+
+    int l = count_node(root->left);
+    int r = count_node(root->right);
+    return l + r + 1;
+}
+
+int main()
+{
+  
+    Node* root = input_order();
+    levelOrder(root);
+    int cnt = count_node(root);
+    cout << cnt << endl;
+    return 0;
+}
---
+++ Module - 18 - Operarions of Binary Tree/level_order_traversel.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+class Node{
+    public:
+    int val;
+    Node* left;
+    Node* right;
+    
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void levelOrder(Node* root){
+
+  queue<Node* > q;
+  q.push(root);
+  while(!q.empty()){
+    Node* f = q.front();
+    q.pop();
+    cout << f->val << " ";
+    if(f->left) q.push(f->left);
+    if(f->right) q.push(f->right);
+  }
+}
+
+
+
+
+int main()
+{
+    Node* root = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+    Node* e = new Node(60);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = e;
+
+    levelOrder(root);
+    return 0;
+}
